d419fa8bca3d80f42e8cbff93117ed8b8c0411cf
anycookie112 <joshuatan2000@hotmail.com>
2024-12-04 11:07:26 +0800

    styling with bootstrap components

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
 from dash import Dash, html, dcc, dash_table
+import dash_bootstrap_components as dbc
 
 import plotly.express as px
 import plotly.subplots as sp
@@ -14,20 +15,38 @@ import pandas as pd
 from sqlalchemy import create_engine
 from datetime import date
 
-app = Dash(__name__, use_pages=True)
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Set the default page to the home page
 app.layout = html.Div([
+
     html.H1('Secondary Rejection %'),
+
+    # Create Navbar with dynamic page links
     html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
+
+        dbc.NavbarSimple(
+            children=[
+                # Dynamically generate links from dash.page_registry
+                dbc.NavItem(
+                    dcc.Link(
+                        page['name'],  # Page name
+                        href=page["relative_path"],  # Page URL
+                        className="nav-link text-light",  # Apply Bootstrap styling
+                    )
+                ) for page in dash.page_registry.values()
+            ],
+            brand="My App",  # Navbar brand
+            brand_href="/",  # Link to home page
+            color="primary",  # Navbar color
+            dark=True  # Dark mode styling for the navbar
+        ),
     ]),
+
+    # Page content goes here (dash.page_container)
     dash.page_container
 ])
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Run the server
+if __name__ == "__main__":
+    app.run_server(debug=True)
